6ce94de1663e86bebbf913e74fdee55b18b63934
Edgar Mendonca <mendonca.ed123@gmail.com>
2024-03-01 03:15:39 +0530

    UPDATE

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// Get the rocket element
+var rocket = document.getElementById("rocket");
+
+// Initially hide the rocket
+rocket.style.display = "none";
+
+// When the user scrolls down 25px from the top of the document, show the rocket
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (window.scrollY > 25) {
+        rocket.style.display = "block";
+    } else {
+        rocket.style.display = "none";
+    }
+}
+
+// When the user clicks on the rocket, scroll to the top of the document and animate the rocket
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+
+
+
+
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
+});
